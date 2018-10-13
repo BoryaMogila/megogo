ddac6ab098da19d0e60d05c92e8659ee7bf13a6a
Bohdan <khokhych@email.ua>
2018-10-14 02:34:18 +0300

    add

--- public/js/main.js
+++ public/js/main.js
@@ -18,20 +18,6 @@
 	headerPopBg.addEventListener('click', mobPopupFunc);
 
 
-<<<<<<< HEAD
-$(document).ready(function () {
-	$('#signIn').on('click', function (e) {
-		$("#widget_10").toggleClass("hide");
-		$("body").toggleClass('open_modal_pop')
-	})
-	$('#registration').on('click', function (e) {
-		$("#form-action-login").toggleClass("hide")
-		$("#form-action-registration").toggleClass("hide")
-		$(".auth-another-action").toggleClass("hide")
-		$("#form-control-age").toggleClass("hide")
-		$(".form-control-sex").toggleClass("hide")
-	})
-=======
 
 
 }())
@@ -54,34 +40,33 @@ $(document).ready(function () {
 // }())
 
 
-$( document ).ready(function() {
-    $('#form-login').on('submit', function (e) {
-        e.preventDefault()
-        var queryParams = $( this ).serialize()
-        $.ajax({
-            method:"get",
-            url: "/user",
-            data: queryParams
-        }).done(function(resp) {
-            if(resp.status &&  resp.status == 200){
-                $('#signIn').toggleClass("hide")
-                $('#signOut').toggleClass("hide")
-            }
-            $("#widget_10").toggleClass("hide")
-        });
+$(document).ready(function () {
+	$('#form-login').on('submit', function (e) {
+		e.preventDefault()
+		var queryParams = $(this).serialize()
+		$.ajax({
+			method: "get",
+			url: "/user",
+			data: queryParams
+		}).done(function (resp) {
+			if (resp.status && resp.status == 200) {
+				$('#signIn').toggleClass("hide")
+				$('#signOut').toggleClass("hide")
+			}
+			$("#widget_10").toggleClass("hide")
+		});
 
-    })
-    $('#signOut').on('click', function (e) {
-        $('#signOut').toggleClass("hide")
-        $('#signIn').toggleClass("hide")
-        $.removeCookie("user_id")
-    })
-    $('#registration').on('click', function (e) {
-        $("#form-action-login").toggleClass("hide")
-        $("#form-action-registration").toggleClass("hide")
-        $(".auth-another-action").toggleClass("hide")
-        $("#form-control-age").toggleClass("hide")
-        $(".form-control-sex").toggleClass("hide")
-    })
->>>>>>> 98c96e65c830c18b29c2a7d91f688f44005cfa54
+	})
+	$('#signOut').on('click', function (e) {
+		$('#signOut').toggleClass("hide")
+		$('#signIn').toggleClass("hide")
+		$.removeCookie("user_id")
+	})
+	$('#registration').on('click', function (e) {
+		$("#form-action-login").toggleClass("hide")
+		$("#form-action-registration").toggleClass("hide")
+		$(".auth-another-action").toggleClass("hide")
+		$("#form-control-age").toggleClass("hide")
+		$(".form-control-sex").toggleClass("hide")
+	})
 })
