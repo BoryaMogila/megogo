81ef5484c277f81191ba1056cc77f129f58b18c2
boryam <borys.mohyla@ria.com>
2018-10-14 11:08:03 +0300

    add ejs

--- app/controllers/SelectGenerController.js
+++ app/controllers/SelectGenerController.js
@@ -2,7 +2,7 @@ const geners = require('../../ganers');
 
 async function indexAction (ctx) {
   console.log(geners)
-  await ctx.render('geners');
+  await ctx.render('geners', { geners });
 }
 
 module.exports = { indexAction };
