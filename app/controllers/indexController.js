81a60fecd3fd8a4ac51d2f807022cb87ecaf61cd
halk895 <iam@851&>
2018-10-14 11:45:58 +0300

    modal

--- app/controllers/indexController.js
+++ app/controllers/indexController.js
@@ -1,17 +1,31 @@
 const myDb = require('../managers/testDbManager'),
-    _ = require('underscore')
+    _ = require('underscore'),
+      axios = require("axios");
 
 
 
 async function indexAction (ctx) {
+    console.log("sdasdasdasd")
+    var popularFimsResp = await axios(`http://api.hackathon.media/video`);
+    popularFimsResp = popularFimsResp.data.data.video_list;
+    var popularFims = []
+    _.map(popularFimsResp, function (item) {
+        if(item.year && year == 2018){
+            popularFims.push({id: item.id, image: item.image, year: item.year, title: item.title})
+        }
+        return popularFims
+    })
+
     let PSP_ID = ctx.cookies.get("psp_id")
+
     if(!PSP_ID){
         let respWebId = await myDb.getWebId();
         if(respWebId && respWebId.insertId){
             ctx.cookies.set("psp_id", respWebId.insertId,{ httpOnly: false })
         }
     }
-
+    //http://api.hackathon.media/video
+ ctx.body = popularFims
  await ctx.render('index');
 }
 
