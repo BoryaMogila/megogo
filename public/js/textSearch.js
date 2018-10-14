423ba179e7fb3dfea537f49cb89c9282c76c5c11
boryam <borys.mohyla@ria.com>
2018-10-14 16:07:22 +0300

    add ejs

--- public/js/textSearch.js
+++ public/js/textSearch.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   $('#search').on('keypress', function (e) {
     $.get('/api/love-movie-search' + window.location.search + '&text=' + $('#search').val(), function (res) {
       var items = res.map(function (video, i) {
-        return '<input type="checkbox" name="films" value="' + video.id + '" class="view_itm-in" id="video_item_' + i + '">\
+        return '<input type="checkbox" name="films[]" value="' + video.id + '" class="view_itm-in" id="video_item_' + i + '">\
                 <label for="video_item_' + i + '" class="view_itm">\
                             <div class="view_itm-img">\
                                 <span class="view_itm-link">\
